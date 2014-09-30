2d7c4b5642748479cf58b880e750b28f31010e22
Douglas Gray <dbgray01@gmail.com>
2014-09-30 23:59:50 +0100

    Initial Commit

---
+++ ESN/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ESN
+//
+//  Created by admin on 30/09/2014.
+//  Copyright (c) 2014 AerophobicEmpurplement. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
